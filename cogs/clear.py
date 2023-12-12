199a960fe3d1523ebe340d774b2c629553177b86
QuOi <152763895+QuOiTeam@users.noreply.github.com>
2023-12-12 22:29:35 +0300

    Update clear.py

--- cogs/clear.py
+++ cogs/clear.py
@@ -16,7 +16,7 @@ class Clear(commands.Cog):
         await interaction.channel.purge(limit=amount + 1)
         
         embed = disnake.Embed(title="Clear", description=f"Удалено {amount} сообщений", color=990000)
-        embed.set_thumbnail(url="https://media.discordapp.net/attachments/1150101252093509635/1184141878225023028/CLEAR-12-12-2023.png?ex=658ae510&is=65787010&hm=12029b7425701bab5ff18f83e2818a6b43cef9da328c0222d2eb54bdc6bbff6d&=&format=webp&quality=lossless&width=1020&height=398")
+        embed.set_thumbnail(url="...")
         await interaction.response.send_message(embed=embed, ephemeral=True)
         
         
